ebd1bba36e3b4004e6073799788d8d0312835ee2
Dilmith Wathsala <it21334306@my.sliit.lk>
2025-07-08 00:15:18 +0530

    added packages routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import DestinationList from './components/Destinations/DestinationList';
 import Navbar from './components/NavBar/NavBar';
 import Home from './components/Home';
 import HotelList from './components/Hotels/HotelList';
+import PackageList from './components/Packages/PackageList';
 import './App.css'; // Assuming you have some global styles
 //import './Home.css'; // Importing Home specific styles
 import './components/NavBar/NavBar.css'; // Importing Navbar specific styles
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/destinations" element={<DestinationList />} />
           <Route path="/hotels" element={<HotelList />} />
+          <Route path='/packages' element={<PackageList />} />
         </Routes>
       
     </BrowserRouter>
