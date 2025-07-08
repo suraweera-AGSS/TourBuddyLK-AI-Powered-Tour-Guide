2736205d1ffa9e7690af4b8f5f5713831e810168
Dilmith Wathsala <it21334306@my.sliit.lk>
2025-07-09 01:33:30 +0530

    added home button

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -5,6 +5,7 @@ import './NavBar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <Link to="/" className="hover:text-yellow-300">Home</Link>
       <Link to="/destinations" className="hover:text-yellow-300">Destinations</Link>
       <Link to="/hotels" className="hover:text-yellow-300">Hotels</Link>
       <Link to="/packages" className="hover:text-yellow-300">Packages</Link>
